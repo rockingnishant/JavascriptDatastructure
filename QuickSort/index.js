3be7372c439dce42b28dd89c4d866b50b676f4f4
singh_nishant <nishantnarayansingh135@gmail.com>
2019-04-06 17:31:56 +0530

    MergeSort and QuickSort

---
+++ QuickSort/index.js
@@ -0,0 +1,24 @@
+//Quick Sort is a divide and conquer algorithm. It first divide the 
+
+let quickSort = (arr) => {
+
+    if (arr.length <= 1) {
+        return arr;
+    } else {
+        let [pivot, ...rest] = arr;
+        let left = [], right = [];
+
+        rest.forEach(element => {
+            element < pivot ? left.push(element) : right.push(element);
+        });
+
+        return quickSort(left).concat(pivot).concat(quickSort(right));
+
+    }
+
+}
+let array = [20, 10, 30, 5, 7, 40, 60];
+let sortedArray = quickSort(array);
+
+console.log("Original Array", array);
+console.log("Sorted Array", sortedArray);
